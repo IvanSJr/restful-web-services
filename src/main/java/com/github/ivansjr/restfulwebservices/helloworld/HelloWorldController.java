7941e68a5769e2386667ae19c5951f2a0fc96309
Ivan Júnior <ivan.junior2706@gmail.com>
2022-08-28 21:01:07 -0300

    Exploring Internationalization for REST API

--- src/main/java/com/github/ivansjr/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/github/ivansjr/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.github.ivansjr.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 // REST API
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     // / hello-world
     @GetMapping(value = "/hello-world")
@@ -24,5 +32,12 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean(@PathVariable String name){
         return new HelloWorldBean("Hello World " + name);
     }
+
+    @GetMapping(value = "/hello-world-internationalization")
+    public String helloWorldInternationalization(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return  messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+    }
+
 
 }
