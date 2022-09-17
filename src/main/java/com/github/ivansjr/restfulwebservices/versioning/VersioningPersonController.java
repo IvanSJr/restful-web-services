cebeafb80d70d17c21ba2d6a7355fbb64aeadcee
Ivan Júnior <ivan.junior2706@gmail.com>
2022-09-17 01:37:39 -0300

    Versioning REST API - Request Param, Header and Content Negotiation

--- src/main/java/com/github/ivansjr/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/github/ivansjr/restfulwebservices/versioning/VersioningPersonController.java
@@ -16,4 +16,33 @@ public class VersioningPersonController {
         return new PersonV2(new Name("Ivan", "Santos"));
     }
 
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
+        return new PersonV1("Ivan Santos");
+    }
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter() {
+        return new PersonV2(new Name("Ivan", "Santos"));
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeaders() {
+        return new PersonV1("Ivan Santos");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeaders() {
+        return new PersonV2(new Name("Ivan", "Santos"));
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeaders() {
+        return new PersonV1("Ivan Santos");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeaders() {
+        return new PersonV2(new Name("Ivan", "Santos"));
+    }
+
 }
